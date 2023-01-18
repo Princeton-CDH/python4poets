9b1710e91d5a19287bf8d2e18167d0fcf7f3184f
Ryan Heuser <ryan.heuser@gmail.com>
2023-01-18 13:33:58 -0500

    adding notebooks and reorging

---
+++ utils.py
@@ -0,0 +1,33 @@
+import re
+
+def tokenize(text):
+    """
+    Split a text into tokens (words, morphemes we can separate such as
+    "n't", and punctuation).
+    """
+    return list(_tokenize_gen(text))
+
+
+def _tokenize_gen(text):
+    import nltk
+    for sent in nltk.sent_tokenize(text):
+        for word in nltk.word_tokenize(sent):
+            yield word
+
+
+def untokenize(words):
+    """
+    Untokenizing a text undoes the tokenizing operation, restoring
+    punctuation and spaces to the places that people expect them to be.
+    Ideally, `untokenize(tokenize(text))` should be identical to `text`,
+    except for line breaks.
+    """
+    text = ' '.join(words)
+    step1 = text.replace("`` ", '"').replace(" ''", '"').replace('. . .', '...')
+    step2 = step1.replace(" ( ", " (").replace(" ) ", ") ")
+    step3 = re.sub(r' ([.,:;?!%]+)([ \'"`])', r"\1\2", step2)
+    step4 = re.sub(r' ([.,:;?!%]+)$', r"\1", step3)
+    step5 = step4.replace(" '", "'").replace(" n't", "n't").replace(
+        "can not", "cannot")
+    step6 = step5.replace(" ` ", " '")
+    return step6.strip()
