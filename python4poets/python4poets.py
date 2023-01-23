d120e0a8414c41ef5e8e7eec97698c292a524209
Ryan Heuser <ryan.heuser@gmail.com>
2023-01-23 12:05:46 -0500

    austen potter

--- python4poets/python4poets.py
+++ python4poets/python4poets.py
@@ -11,8 +11,8 @@ except LookupError:
 cache_nouns = None
 
 ### strings
-austen_str = 'It is a truth universally acknowledged, that a single man in possession of a good fortune, must be in want of a wife.'
-potter_str = '''Harry had a thin face, knobbly knees, black hair and bright-green eyes. He wore round glasses held together with a lot of Sellotape because of all the times Dudley had punched him on the nose. The only thing Harry liked about his own appearance was a very thin scar on his forehead which was shaped like a bolt of lightning.'''
+austen = austen_str = 'It is a truth universally acknowledged, that a single man in possession of a good fortune, must be in want of a wife.'
+potter = potter_str = '''Harry had a thin face, knobbly knees, black hair and bright-green eyes. He wore round glasses held together with a lot of Sellotape because of all the times Dudley had punched him on the nose. The only thing Harry liked about his own appearance was a very thin scar on his forehead which was shaped like a bolt of lightning.'''
 
 
 ### functions
